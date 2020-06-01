e2d94e2898356bae1f89f7fe714e04873d845e93
mdmaruf43 <marufahmedcse76@gmail.com>
2020-06-01 21:37:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = process.env.DB_PATH;
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.get('/products', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("powerXgymStore").collection("category");
+        collection.find().toArray((err, documents) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+});
+
+
+app.post('/addProduct', (req, res) => {
+    //save to database
+    const product = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("powerXgymStore").collection("category");
+        collection.insert(product, (err, result) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
+})
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Listing to port 4000'));
